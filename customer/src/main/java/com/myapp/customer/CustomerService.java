f8f81b689606c3f505eaf685d52a178ba11b0ab0
Rabia <sdetcode@gmail.com>
2022-01-22 12:29:58 +0530

    first commit

---
+++ customer/src/main/java/com/myapp/customer/CustomerService.java
@@ -0,0 +1,11 @@
+package com.myapp.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer=Customer.builder().firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName()).email(customerRegistrationRequest.email()).build();
+    }
+}
